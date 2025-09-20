0a8f22673cd1f24e0fd9884d8d482dea9cf12902
atomon <38009289+atomon@users.noreply.github.com>
2025-09-21 02:06:18 +0900

    debug

--- generator/nanopb_generator.py
+++ generator/nanopb_generator.py
@@ -74,6 +74,9 @@ else:
     # by env variable NANOPB_PB2_NO_REBUILD
     nanopb_pb2 = proto.load_nanopb_pb2()
 
+breakpoint()
+print(nanopb_pb2)
+print(nanopb_pb2.IS_8)
 # ---------------------------------------------------------------------------
 #                     Generation of single fields
 # ---------------------------------------------------------------------------

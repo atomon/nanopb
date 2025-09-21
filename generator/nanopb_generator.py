470bd67735d588593ac8c8872c9891c07e978e13
atomon <38009289+atomon@users.noreply.github.com>
2025-09-21 11:24:53 +0900

    debug

--- generator/nanopb_generator.py
+++ generator/nanopb_generator.py
@@ -74,10 +74,10 @@ else:
     # by env variable NANOPB_PB2_NO_REBUILD
     nanopb_pb2 = proto.load_nanopb_pb2()
 
-if nanopb_pb2.IS_8 is None:
-    sys.stderr.write("False")
-else:
-    sys.stderr.write(f"{nanopb_pb2.IS_8}")
+    if nanopb_pb2.IS_8 is None:
+        sys.stderr.write("False")
+    else:
+        sys.stderr.write(f"{nanopb_pb2.IS_8}")
 
 sys.stderr.write(f"{dir(nanopb_pb2)}")
 # ---------------------------------------------------------------------------
